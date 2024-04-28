8efbf53bbd13253e82ad431f080c72892dde1b5e
Mahdi Fallah <137988053+mmf86@users.noreply.github.com>
2024-04-29 01:31:10 +0330

    G

--- Online2/G/q.py
+++ Online2/G/q.py
@@ -1,34 +1,27 @@
-import sys
-import numpy as np
-
-def solve(P, Q):
-    n = P.shape[0]
-    INF = 1 << 60
-    L = np.full(n + 1, INF, np.int64)
-    ind = np.argsort(Q)
-    for i in range(n):
-        p = P[i]
-        M = np.arange(p, n + 1, p)
-        idx = np.sort(ind[M - 1])
-        for d in idx[::-1]:
-            k = np.searchsorted(L, d)
-            L[k] = d
-    ans = np.searchsorted(L, INF)
-    return ans
-
-def main():
-    n = int(input())
-    P = np.fromstring(input(), np.int64, sep=' ')
-    Q = np.fromstring(input(), np.int64, sep=' ')
-    print(solve(P, Q))
+import bisect
+N = int(input())
+P = list(map(int, input().split()))
+Q = list(map(int, input().split()))
+pos = [0]*(N+1)
+for i in range(N):
+    pos[Q[i]] = i
 
 
-if __name__ == '__main__':
-    if sys.argv[-1] == 'ONLINE_JUDGE':
-        from numba.pycc import CC
-        cc = CC('my_module')
-        cc.export('solve', 'i8(i8[:], i8[:])')(solve)
-        cc.compile()
-        exit()
-    from my_module import solve
-    main()
+can_use = []
+for i in range(N):
+    tmp = []
+    for j in range(P[i], N+1, P[i]):
+        tmp.append(pos[j])
+    tmp.sort(reverse=True)
+    can_use.append(tmp)
+
+dp = [-1]
+
+for i in range(N):
+    for x in can_use[i]:
+        if x > dp[-1]:
+            dp.append(x)
+        else:
+            dp[bisect.bisect_left(dp, x)] = x
+
+print(len(dp[1:]))
